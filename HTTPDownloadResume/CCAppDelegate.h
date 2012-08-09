49915591597640ef4f5a26630d86457e0fdb01de
zfqxy <ibokan@zfqxy.local>
2012-08-09 11:16:23 +0800

    Initial Commit

---
+++ HTTPDownloadResume/CCAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  CCAppDelegate.h
+//  HTTPDownloadResume
+//
+//  Created by Ibokan on 12-8-9.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class CCViewController;
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) CCViewController *viewController;
+
+@end
---
+++ HTTPDownloadResume/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  HTTPDownloadResume
+//
+//  Created by Ibokan on 12-8-9.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
